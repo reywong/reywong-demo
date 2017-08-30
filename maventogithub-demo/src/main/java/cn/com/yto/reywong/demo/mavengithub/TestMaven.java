e9e4873bb729382c62b086debcaf0b6487189b83
reywongc@163.com <reywongc@163.com>
2017-08-30 10:53:57 +0800

    修改pom文件

---
+++ maventogithub-demo/src/main/java/cn/com/yto/reywong/demo/mavengithub/TestMaven.java
@@ -0,0 +1,8 @@
+package cn.com.yto.reywong.demo.mavengithub;
+
+public class TestMaven {
+    public static void main(String[] args) {
+        String ss="11";
+//        if(StringUtils)
+    }
+}
--- ssm/reywong-common/reywong-common-framework/src/main/java/cn/com/yto56/coresystem/common/stl/framework/domain/RequestParamBean.java
+++
@@ -1,9 +0,0 @@
-package cn.com.yto56.coresystem.common.stl.framework.domain;
-
-/**
- * Created by wangrui on 2017/8/15.
- */
-public class RequestParamBean {
-    private String sign;
-
-}
